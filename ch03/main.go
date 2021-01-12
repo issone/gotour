0759f0243ac4dcf571d3c2fe6ab50d474866c189
issone <269773075@qq.com>
2021-01-12 13:59:14 +0800

    补充switch

--- ch03/main.go
+++ ch03/main.go
@@ -118,3 +118,66 @@ func test7() {
 	}
 	fmt.Println("the sum is", sum)
 }
+
+// switch 后可以不接任何变量、表达式、函数。
+// 当不接任何东西时，switch - case 就相当于 if - elseif - else
+func test8() {
+
+	score := 30
+
+	switch {
+	case score >= 95 && score <= 100:
+		fmt.Println("优秀")
+	case score >= 80:
+		fmt.Println("良好")
+	case score >= 60:
+		fmt.Println("合格")
+	case score >= 0:
+		fmt.Println("不合格")
+	default:
+		fmt.Println("输入有误...")
+	}
+}
+
+// case 后可以接多个多个条件，多个条件之间是 或 的关系，用逗号相隔。
+func test9() {
+	month := 2
+
+	switch month {
+	case 3, 4, 5:
+		fmt.Println("春天")
+	case 6, 7, 8:
+		fmt.Println("夏天")
+	case 9, 10, 11:
+		fmt.Println("秋天")
+	case 12, 1, 2:
+		fmt.Println("冬天")
+	default:
+		fmt.Println("输入有误...")
+	}
+}
+
+
+// 判断一个同学是否有挂科记录的函数,返回值是布尔类型
+func getResult(args ...int) bool {
+    for _, i := range args {
+        if i < 60 {
+            return false
+        }
+    }
+    return true
+}
+
+// switch 后面可以接一个函数，只要保证 case 后的值类型与函数的返回值 一致即可。
+func test10() {
+    chinese := 80
+    english := 50
+    math := 100
+
+    switch getResult(chinese, english, math) {
+    // case 后也必须 是布尔类型
+    case true:
+        fmt.Println("该同学所有成绩都合格")
+    case false:
+        fmt.Println("该同学有挂科记录")
+}
